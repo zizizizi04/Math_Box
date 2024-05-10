fe40cf65b40c65043caac47be0abf7b6868d07ad
shin <mineem1212@gmail.com>
2024-05-10 22:26:34 +0900

    Add factorial function

--- calculator.py
+++ calculator.py
@@ -14,7 +14,6 @@ def divide_new(a, b):
 def get_Median(a, b):
 	return (a+b)/2
 
-<<<<<<< HEAD
 def get_Remainder(a, b):
 	return a//b
 
@@ -30,3 +29,9 @@ def get_Percent(a, b):
 def get_Sum_ver1(n):
 	return n(n+1)/2
 
+def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
